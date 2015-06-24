c721329dd63ce42dc79d8ccd1d8b6d72db5475fb
Niko Roberts <niko@tasboa.com>
2015-06-24 13:06:00 +1000

    made each formatter required instead

--- lib/grape_logging.rb
+++ lib/grape_logging.rb
@@ -1,4 +1,5 @@
 require 'grape_logging/multi_io'
 require 'grape_logging/version'
-require 'grape_logging/formatters/*'
+require 'grape_logging/formatters/default'
+require 'grape_logging/formatters/logstash'
 require 'grape_logging/middleware/request_logger'
