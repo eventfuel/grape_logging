df2f2706febea59ce13cc5893b3c87a7f91ef213
Niko Roberts <niko@tasboa.com>
2015-06-24 13:03:28 +1000

    made all formatters required

--- lib/grape_logging.rb
+++ lib/grape_logging.rb
@@ -1,4 +1,4 @@
 require 'grape_logging/multi_io'
 require 'grape_logging/version'
-require 'grape_logging/formatters/default'
+require 'grape_logging/formatters/*'
 require 'grape_logging/middleware/request_logger'
